7c23b3734455b4bf69f826d17fc00a6752486396
Cezary Suchorski <cezary.suchorski@wp.pl>
2022-03-12 20:20:47 +0100

    Added Database and data seeding method

--- ContosoUniversity/Program.cs
+++ ContosoUniversity/Program.cs
@@ -1,5 +1,7 @@
+using ContosoUniversity.Data;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,7 +15,19 @@ namespace ContosoUniversity
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            CreateDbIfNeeded(host);
+            host.Run();
+        }
+
+        public static void CreateDbIfNeeded(IHost host)
+        {
+            using(var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var context = services.GetRequiredService<SchoolContext>();
+                DbInitializer.Initialize(context);
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
