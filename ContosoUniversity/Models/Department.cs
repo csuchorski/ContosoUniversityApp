4c2b0c1c26a0a91e61ba6eabaa6a3e4e8c37dd65
csuchorski <cezary.suchorski@wp.pl>
2022-04-19 16:23:19 +0200

    Added rowversion to department model and migrated the changes to db

--- ContosoUniversity/Models/Department.cs
+++ ContosoUniversity/Models/Department.cs
@@ -27,6 +27,8 @@ namespace ContosoUniversity.Models
         public DateTime StartDate { get; set; }
         public int? InstructorID { get; set; }
 
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
         public Instructor Administrator { get; set; }
         public ICollection<Course> Courses { get; set; }
     }
